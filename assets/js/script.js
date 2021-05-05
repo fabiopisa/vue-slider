6ac24ef8860bdf5d6fd6b23dd1428217d4bbe31d
fabiopisa <pisanellifabio@gmail.com>
2021-05-05 15:42:36 +0200

    commit esercizio finito senza bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,30 @@
 Rifare l’esercizio dello slider come fatto assieme in classe.
 Bonus:
 Applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente. */
+
+const app = new Vue({
+
+  el:'#app',
+  data:{
+
+    positionPhoto: 0,
+    photos:[
+      'assets/img/im1.jpg',
+      'assets/img/im6.jpg',
+      'assets/img/im8.jpg',
+      'assets/img/im9.jpg'
+    ],
+
+  },
+  methods:{
+
+    prevPhoto(){
+      this.positionPhoto--;
+      if(this.positionPhoto < 0) this.positionPhoto = this.photos.length - 1;
+    },
+    nextPhote(){
+      this.positionPhoto++;
+      if(this.positionPhoto === this.photos.length) this.positionPhoto = 0;
+    }
+  }
+})
